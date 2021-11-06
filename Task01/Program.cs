c5359af47bd06387467b52c789f5ae6729ade0e9
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-06 16:40:49 +0300

    Lesson 01 Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿/* По двум заданным числам проверять является ли первое квадратом второго */
+
+double a=64,b;
+string s = Console.ReadLine();
+b = Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(b,2));
+if(a == Math.Pow(b,2))
+{
+    Console.WriteLine(a + " является квадратом числа " + (b));
+} 
+else
+{
+    
+    Console.WriteLine(a + " не является квадратом числа " + (b));
+}
