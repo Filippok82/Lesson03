8d6acb373cb289aa5203cb018ae8b67507fd63c1
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-07 16:15:03 +0300

    Lesson03 Task05 and Task06

---
+++ Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿// 5. Написать программу вычисления значения функции y=f(a)
+double y, f, a;
+Console.WriteLine(" введите значение f ");
+f = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" введите значение a ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(y = f*a);
+
+
---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿// 6. Выяснить является ли число чётным
+int s;
+Console.WriteLine("Введите число ");
+s= Convert.ToInt32(Console.ReadLine ());
+if (s%2==0)
+{
+    Console.WriteLine(s + " число является четным");
+}
+else
+{
+    Console.WriteLine("число не четное");
+}
