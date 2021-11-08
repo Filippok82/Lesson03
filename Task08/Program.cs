0a2992d47481a3c3712bb2fa3aeeb06ed762a2d2
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-08 14:24:54 +0300

    Lesson03 Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.WriteLine("Введите число N ");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+
+int i;
+for (i = 0; i <= n; ++i)
+{
+    if (i % 2 == 0)
+    {
+    Console.Write(i + " ");
+    }
+    
+}
