d276bd7b54e48ba57e9e8576a816ca04c88804db
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-08 14:49:42 +0300

    Lesson03 Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,6 @@
+﻿//  9. Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите техзначное число");
+int c,a;
+a = Convert.ToInt32(Console.ReadLine());
+c = a % 10;
+Console.WriteLine("Последня цифра числа  " + a + " " + c);
