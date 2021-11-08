f2bf616d86ff621eae5498415315640f566def32
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-08 18:36:12 +0300

    Lesson03 Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int n = new Random().Next(10,99);
+Console.WriteLine(n);
+int s = n%10;
+int f = (n - s)/10;
+if (f > s)
+{
+    Console.WriteLine(f + " наибольшая цифра числа " + n);
+}
+else 
+{ 
+    Console.WriteLine(s + " наибольшая цифра числа " + n);
+}
