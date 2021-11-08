3dc72904e8a82000c407a2de40260ab51132e1ef
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-08 13:37:09 +0300

    Lesson03 Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿//7. Показать числа от -N до N
+
+Console.WriteLine("Введите число -N");
+int minN;
+minN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int maxN;
+maxN = Convert.ToInt32(Console.ReadLine());
+
+
+int index;
+index = minN;
+while (index <= maxN)
+{
+    Console.Write(index + " ");
+    index++;
+}
