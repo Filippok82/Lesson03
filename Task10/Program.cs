3db815791f4646fb0419bbab5432cf1e7ec22b81
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-08 17:49:03 +0300

    Lesson03 Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите техзначное число");
+int c,a;
+a = Convert.ToInt32(Console.ReadLine());
+c = (a % 100)/10;
+Console.WriteLine("Вторая цифра числа  " + a + " " + c);
