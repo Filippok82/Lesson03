62a6c90a3c68de0283f7d564ac922d005df2344a
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-09 15:53:07 +0300

    Lesson03 Task14 Task15 Task22

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+int a;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if(a%7==0|a%23==0)
+{
+    Console.WriteLine("Данное число кратно 7 и 23!");
+    
+}
+else {Console.WriteLine("Условие не выполняется.");}
---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D
+
+ double Distance2D(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+Console.WriteLine(Distance2D(0, 0, 2, 2)); 
+ 
+// 22. Найти расстояние между точками в пространстве 3D
+
+double Distance3D(double x1, double y1, double z1, double x2, double y2, double Z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(Z2 - z1, 2));
+}
+Console.WriteLine(Distance3D(0, 0, 0, 2, 2, 2));
