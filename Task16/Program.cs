b2e1844a0e399f339b0f87c60c6aedaef8118e15
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-10 17:59:49 +0300

    Lesson03 Task16 Task17

---
+++ Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+
+int s; 
+Console.WriteLine ("Введите число от 1 до 7:  ");
+s = Convert.ToInt32(Console.ReadLine());
+
+if (s < 6) Console.WriteLine("Будни");
+if (s >= 6) Console.WriteLine("Выходной");
+
+
---
+++ Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+double s,a;
+Console.WriteLine("Ввведите первое число:" );
+s = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите второе число:" );
+a = Convert.ToInt32(Console.ReadLine());
+double s1 = Math.Pow(s,2);
+double a1 = Math.Pow(a,2);
+
+
+if (s == a1)
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+
+if (a == s1)
+{
+  Console.WriteLine("Первое число является квадратом второго"); 
+}
+else
+{
+  Console.WriteLine("Число не являются квадратом другого");   
+}
