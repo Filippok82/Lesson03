fae79c657500daa984c525684f134b9482f865df
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-13 20:18:45 +0300

    Lesson04 Task35 Домашнее задание

---
+++ Task35/Program.cs
@@ -0,0 +1,20 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+ int[] array = new int[25];
+Console.WriteLine("Какое число будем искать?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 31);
+    Console.Write(array[i] + " ");
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (n == array[i])
+    {
+        Console.WriteLine("Число " +  n  + " найдено");
+    }
+            
+}
+    
