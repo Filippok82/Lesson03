f4f42e3e2aa6d7524f16b03cc77c8e84be6703d2
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-17 22:08:11 +0300

    Lesson 03 Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿//30. Показать кубы чисел, заканчивающихся на четную цифру
+double[] arr = new double[10];
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+}
+double Cub = 0;
+double a = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    
+    Cub = Math.Pow(arr[i],3);
+    Console.WriteLine(Cub);
+
+    a = Cub%10;
+     /* Console.WriteLine(a); */
+     if(a%2==0)
+     {
+         Console.WriteLine( "Заканчивается четным число : " + a);
+     }
+}
