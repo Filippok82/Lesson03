382bb911d874a1ecbecf25d13f82af98a3a7aa5a
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-17 13:50:23 +0300

    Lesson05 Task 20 Task37

---
+++ Task20/Program.cs
@@ -0,0 +1,9 @@
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Выберите  номер четверти №1; №2; №3; №4");
+double x; 
+x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 2) Console.WriteLine("Диапазон возможных координат x < 0 & Y > 0");
+if (x == 1) Console.WriteLine("Диапазон возможных координат x > 0 & Y > 0");
+if (x == 3) Console.WriteLine("Диапазон возможных координат x < 0 & Y < 0");
+if (x == 4) Console.WriteLine("Диапазон возможных координат x > 0 & Y < 0");
