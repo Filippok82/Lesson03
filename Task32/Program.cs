4874e8ad7b5e6faf43914fc148977605afd49a4a
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-18 10:10:35 +0300

    Lesson03 Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,14 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+int [] array = new int [8];
+Random random = new Random();
+for(int i=0;i< array.Length;i++)
+{
+   array[i]= random.Next(0,2);
+   Console.WriteLine( array[i]);
+}
+
+
+
+
+
+
