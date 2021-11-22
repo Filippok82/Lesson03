78effc3db56234d75981016bc289f108528456a6
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-11-22 19:55:23 +0300

    Lesson03  Task42 Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,12 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+
+Console.Write("Введите число :");
+int num =Convert.ToInt32(Console.ReadLine());
+string BinaryCode = Convert.ToString(num, 2);
+
+Console.WriteLine($" В двоичной системе исчесления : {BinaryCode}");
+
+
+
+
