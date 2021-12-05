c9cfa5d7832e0de2423fa255349e0012cfa266a2
Irina Filippova <irina.filippova.mail@yandex.ru>
2021-12-05 16:03:15 +0300

    Lesson03 Task66-Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,13 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+
+void Num (int a)
+{
+    int M =25;
+    int N =52;
+    if(a>=M && a<=N)
+    {
+        Console.Write($"{a}"+ " ");
+        Num (a+1);
+    }
+}
+Num(25);
